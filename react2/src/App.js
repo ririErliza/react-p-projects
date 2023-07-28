bd126397ac536637cdb66af6c3686d5bce3b695a
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-07-28 12:46:34 +0200

    update

--- react2/src/App.js
+++ react2/src/App.js
@@ -1,17 +1,22 @@
+import Create from './component/Create';
 import Home from './component/Home';
 import Navbar from './component/Navbar';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
-
-
   return (
-    <div className='App'>
-      <Navbar/>
-      <div className="content">
-        <Home/>
+    <Router>
+      <div className='App'>
+        <Navbar/>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/create" element={<Create/>}/>
+          </Routes>
+          
+        </div>
       </div>
-
-    </div>
+    </Router>
   );
 }
 
