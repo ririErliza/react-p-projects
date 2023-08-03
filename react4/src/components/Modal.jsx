37fbcc968158d4c355f5991e46a626e3a134e76f
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-08-03 11:52:11 +0200

    update

---
+++ react4/src/components/Modal.jsx
@@ -0,0 +1,14 @@
+const Modal = () => {
+  return (
+    <aside className="modal-container">
+      <div className="modal">
+        <h4>remove all items from your shopping cart?</h4>
+        <div className="btn-container">
+          <button className="btn confirm-btn">confirm</button>
+          <button className="btn clear-button">cancel</button>
+        </div>
+      </div>
+    </aside>
+  );
+};
+export default Modal;
---
+++ react4/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { CartIcon } from "../icons";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>RTK shop</h3>
+        <div className="nav-container">
+          <CartIcon />
+          <div className="amount-container">
+            <p className="total-amount">0</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
